1b9c12779af98edccad1a1952d2e885dcf162eb2
hhhhsss <1637586409@qq.com>
2018-11-20 10:56:43 +0800

    修改歌词滚动的延迟功能.

--- HZMultiMedia/app/src/main/aidl/of/media/hz/Music.aidl
+++
@@ -1,6 +0,0 @@
-// Music.aidl
-package of.media.hz;
-
-// Declare any non-default types here with import statements
-
-parcelable Music;
--- HZMultiMedia/app/src/main/java/of/media/hz/model/RequestCode.java
+++
@@ -1,7 +0,0 @@
-package of.media.hz.model;
-
-public interface RequestCode {
-    int REQUEST_WRITE_SETTINGS = 0;
-    int REQUEST_ALBUM = 1;
-    int REQUEST_CORP = 2;
-}
